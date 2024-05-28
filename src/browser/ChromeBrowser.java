d8e9dacb1f4f2ea4fbcd3a87bcf4eefec3707050
KacaQA <katarina.sindjic@amplitudo.me>
2024-05-28 12:23:03 +0200

    Playing with title and URL

--- src/browser/ChromeBrowser.java
+++ src/browser/ChromeBrowser.java
@@ -6,12 +6,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ChromeBrowser {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        // ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User.DESKTOP-VN57FBP\\Downloads\\chromedriver.exe");
 
-        // Kreiramo Chrome instancu
+        // Onstanca Chrome browsera
         WebDriver driver = new ChromeDriver();
 
+        // Otvaranje stranice
         driver.get("https://amplitudo.me");
+
+        // Dobijanje title-a
+        String pageTitle = driver.getTitle();
+        System.out.println("Title: " + pageTitle);
+        System.out.println("Title length: " + pageTitle.length());
+
+        // Dobijanje URL-a
+        String pageUrl = driver.getCurrentUrl();
+        System.out.println("Page URL: " + pageUrl);
+
+        // Provjera da li je URL siguran
+        if (pageUrl.startsWith("https")) {
+            System.out.println("URL is secure");
+        } else {
+            System.out.println("URL is not secure");
+        }
+
+        // Dobijanje page source-a
+        String pageSource = driver.getPageSource();
+        System.out.println("Page Source Length: " + pageSource.length());
+
+        driver.quit();
     }
 }
