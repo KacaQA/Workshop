334dfc9482d97904ff683ead74ac3968cc29a062
KacaQA <katarina.sindjic@amplitudo.me>
2024-05-31 15:21:05 +0200

    Dodavanje nove klase i fix

--- src/demoqa/DemoqaPractice.java
+++ src/demoqa/DemoqaPractice.java
@@ -12,14 +12,12 @@ import java.util.List;
 
 public class DemoqaPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User.DESKTOP-VN57FBP\\Downloads\\chromedriver.exe");
 
         // Instanca Chrome browsera
         WebDriver driver = new ChromeDriver();
-
-        try {
 
             driver.get("https://demoqa.com/automation-practice-form");
 
@@ -84,11 +82,9 @@ public class DemoqaPractice {
 
             Thread.sleep(3000);
 
-        } catch (Exception e) {
-            System.out.println("Doslo je do greske: " + e.getMessage());
-        } finally {
+
 
             driver.quit();
-        }
+
     }
 }
