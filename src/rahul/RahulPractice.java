3b6e8d879fc6db74eab9b942ce26b8332220fbfd
KacaQA <katarina.sindjic@amplitudo.me>
2024-06-03 09:01:49 +0200

    Rahul list

--- src/rahul/RahulPractice.java
+++ src/rahul/RahulPractice.java
@@ -1,7 +1,11 @@
 package rahul;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class RahulPractice {
     public static void main(String[] args) {
@@ -11,6 +15,37 @@ public class RahulPractice {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        // Select second radio button
+        WebElement radioBtn = driver.findElement(By.xpath("(//input[@name='radioButton'])[2]"));
+        radioBtn.click();
+
+        // Select second option
+        WebElement secondOpt = driver.findElement(By.id("dropdown-class-example"));
+        secondOpt.findElement(By.xpath("//option[3]")).click();
+
+        // Select second checkbox
+        WebElement secondCheck = driver.findElement(By.id("checkBoxOption2"));
+        secondCheck.click();
+
+        // Choose dynamically second row and print it from first table
+        WebElement secondRow = driver.findElement(By.xpath("//table[@name='courses']//tr[3]"));
+        System.out.println("Second row: " + secondRow.getText());
+
+        // Print all positions from the second table
+        List<WebElement> positions = driver.findElements(By.xpath("//div[@class='tableFixHead']//table[@id='product']//td[2]"));
+        System.out.println("Positions from the second table:");
+        for (WebElement position : positions) {
+            System.out.println(position.getText());
+        }
+
+        // Print size of the second list
+        List<WebElement> checkboxList = driver.findElements(By.xpath("//input[@type='checkbox']"));
+        System.out.println("Size of the second list: " + checkboxList.size());
+
+
+        driver.quit();
+
 
     }
 }
