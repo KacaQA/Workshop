90fe3e4565bf05e69929bb4fc077ec79d0fd8b51
KacaQA <katarina.sindjic@amplitudo.me>
2024-06-13 10:49:39 +0200

    Assert

--- src/contact/ContactUs.java
+++ src/contact/ContactUs.java
@@ -1,9 +1,12 @@
 package contact;
 
+
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class ContactUs {
 
@@ -26,6 +29,32 @@ public class ContactUs {
 
         WebElement submitBtn = driver.findElement(By.cssSelector("input[type='submit']"));
         submitBtn.click();
+
+        WebElement thankYou = driver.findElement(By.cssSelector("#contact_reply h1"));
+        String givenMessage = thankYou.getText();
+        Assert.assertEquals(givenMessage, "Thank You for your Message!");
+
+        driver.navigate().back();
+
+        WebElement resetBtn = driver.findElement(By.cssSelector("input[type='reset']"));
+        resetBtn.click();
+
+        submitBtn.click();
+        Thread.sleep(2000);
+
+        // Provjera poruka o greskama
+        WebElement bodyMessage = driver.findElement(By.cssSelector("html"));
+        String givenBodyMessage = bodyMessage.getText();
+        System.out.println("Poruke koje su ispisane : " +givenBodyMessage);
+
+        // Assert poruke o greskama
+        Assert.assertTrue(givenBodyMessage.contains("Error: all fields are required"), "Poruka 'all fields are required' nije prikazana");
+        Assert.assertTrue(givenBodyMessage.contains("Error: Invalid email address"), "Poruka 'Invalid email address' nije prikazana");
+
+        driver.navigate().back();
+
+        driver.quit();
+
 
     }
 }
